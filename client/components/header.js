3dad88b9dc337695c0390ff70b2eb386ec62d98d
Adegbuyi Ademola <marhyorh@gmail.com>
2016-07-23 21:10:21 +0100

    Header Component done!

--- client/components/header.js
+++ client/components/header.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
 import { Link, browserHistory } from 'react-router';
+import { createContainer } from 'meteor/react-meteor-data';
 import Accounts from './accounts';
 
 class Header extends Component {
   render() {
+    const isAuth = () => {
+      if (this.props.userId) {
+        return (
+          <ul className="nav navbar-nav navbar-right">
+            <li className="nav-item">
+              <Link to="/myPins">
+                My Pins
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/add">
+                Add
+              </Link>
+            </li>
+          </ul>
+        );
+      }
+    }
+
     return (
-      <nav className="nav navbar-default">
-        <div className="navbar-header">
-          <Link to="/" className="navbar-brand"> p1nit </Link>
+      <nav className="navbar navbar-fixed-top navbar-default">
+        <div className="container">
+          <div className="navbar-header">
+            <Link to="/" className="navbar-brand"> p1nit </Link>
+          </div>
+          <ul className="nav navbar-nav">
+            <li>
+              <Accounts />
+            </li>
+          </ul>
+          { isAuth() }
         </div>
-        <ul className="nav navbar-nav">
-          <li>
-            <Accounts />
-          </li>
-        </ul>
       </nav>
     );
   }
 };
 
-export default Header;
+export default createContainer(() => {
+  return { userId: Meteor.userId() }
+}, Header);
