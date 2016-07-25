019e22281c454f204a05fb0b6242421a3b6f22ac
Adegbuyi Ademola <marhyorh@gmail.com>
2016-07-25 02:29:07 +0100

    Completed :smile:

---
+++ client/components/user-pin.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { Link, hashHistory } from 'react-router';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Pins } from '../../imports/collections/pins';
+
+class UserPin extends Component {
+  imageBroken(e) {
+    e.target.src = 'https://placeholdit.imgix.net/~text?txtsize=30&txt=p1nit&w=200&h=200';
+  }
+
+  onLike(pin, event) {
+    event.preventDefault();
+
+    Meteor.call('pin.like', pin);
+  }
+
+  onRemoveLike(pin, event) {
+    event.preventDefault();
+
+    Meteor.call('pin.unlike', pin);
+  }
+
+  onRetweet(pin, event) {
+    event.preventDefault();
+
+    Meteor.call('pin.retweet', pin);
+  }
+
+  user(id) {
+    return this.props.users.map(user => {
+      if (user._id === id) {
+        if (user.services.twitter) {
+          return user.services.twitter.screenName;
+        }
+        return user.emails[0].address.split('@')[0];
+      }
+    });
+  }
+
+  renderPins() {
+    return this.props.pins.map(pin => {
+      return (
+        <div className="grid-item" key={pin._id}>
+          <img src={pin.url} className="img-responsive" onError={this.imageBroken.bind(this)} style={{width: "100%"}}/>
+          <div className="padded">
+            <h4>{pin.title}</h4>
+            <h6>{pin.description}</h6>
+            {/*<h6 className="text-muted"><Link to={`/user/${pin.ownerId}`}><i className="fa fa-user" /> {user(pin.ownerId)}</Link></h6>*/}
+            <div className="action-bar">
+              { pin.likedBy.includes(this.props.userId) ? <i className="fa fa-heart green" onClick={this.onRemoveLike.bind(this, pin)}/>  : <i className="fa fa-heart" onClick={this.onLike.bind(this, pin)}/> }
+              { pin.likedBy.length > 0 ? <sup>{pin.likedBy.length}</sup> : ""}
+              { pin.ownerId !== this.props.userId ? <i className="fa fa-retweet" onClick={this.onRetweet.bind(this, pin)} style={{float:"right"}}/> : null }
+            </div>
+          </div>
+        </div>
+      )
+    });
+  }
+
+  render() {
+    if (!this.props.pins) {
+      return <div class="alert alert-info">Oops! This user has no pin </div>
+    }
+
+    return (
+      <div>
+        <h4 style={{textAlign: "center"}}> {this.user(this.props.params.id)} Pins </h4>
+        <div className="all-pins">
+          <div className="grid">
+            { this.renderPins() }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default createContainer( props => {
+  Meteor.subscribe('pins');
+  Meteor.subscribe('userList');
+
+  return { pins: Pins.find({ ownerId: props.params.id }).fetch(), userId: Meteor.userId(), users: Meteor.users.find().fetch() };
+}, UserPin);
--- client/main.js
+++ client/main.js
@@ -5,6 +5,7 @@ import Header from './components/header';
 import Add from './components/add';
 import MyPins from './components/my-pins';
 import All from './components/all';
+import UserPin from './components/user-pin';
 
 const App = props => {
   return (
@@ -22,6 +23,7 @@ const routes = (
     <Route component={App} path="/">
       <IndexRoute component={All} />
       <Route component={MyPins} path="/my-pins" />
+      <Route component={UserPin} path="/user/:id" />
       <Route component={Add} path="/add" />
     </Route>
   </Router>
